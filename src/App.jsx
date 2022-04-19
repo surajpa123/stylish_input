bc9a05267b65f5c2e8e5a59c09648656c65a5311
Suraj Pathak <pathaksuraj136@gmail.com>
2022-04-19 16:00:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import React from 'react';
+import './App.css'
+import { StatefulPinInput } from 'react-input-pin-code';
+
+function App() {
+  const [values, setValues] = React.useState(['', '', '']);
+
+
+  const submit= ()=>{
+    
+    console.log(values)
+    alert("Submitted")
+  }
+
+  return (
+    <div className="App">
+<div className='inputdiv'>
+<h1>Enter Your Pin</h1>
+
+<StatefulPinInput
+className="inputb"
+  initialValue=""
+  size="lg"
+  type='password'
+  onChange={(value, index, values) => setValues(values)}
+/>
+<br />
+<button onClick={submit}>Submit</button>
+
+
+
+</div>
+
+
+
+
+     
+    </div>
+  )
+}
+
+export default App
